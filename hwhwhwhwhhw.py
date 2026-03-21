02c0e8dc8139d6d5b42f646ca35064140f14bdab
rastogidevansh-1838 <rastogi.kopal@gmail.com>
2026-03-21 08:45:17 +0530

    hwhwwhwhwhwhwhwhhw2hwhwww

---
+++ hwhwhwhwhhw.py
@@ -0,0 +1,8 @@
+Set1 = {'blue', 'green'}
+Set2 = {'blue', 'yellow'}
+ResultA = Set1.symmetric_difference(Set2)
+print("Result A:", ResultA)
+Set1 = {1, 2, 3, 4, 5}
+Set2 = {1, 5, 6, 7, 8, 9}
+ResultB = Set1.symmetric_difference(Set2)
+print("Result B:", ResultB)
